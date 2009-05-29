7023268b3e2005f3f917b5879cbb4c78e8842edf
Ondrej Certik <ondrej@certik.cz>
2009-05-29 14:58:28 -0600

    make it run

--- readbin3.py
+++ readbin3.py
@@ -1,3 +1,7 @@
+from math import pi
+
+from numpy import arange, meshgrid, sin, cos
+
 nplanets = 1
 nr = 800
 nphi = 4*nr
@@ -8,9 +12,12 @@ hdr = dr/2
 dp = 2.*pi/nphi
 hdp = dp/2.0
 
-r, phi = meshgrid(0.4+dr*(1:nr)-hdr, 0+dp*(0:nphi)+hdp)
-x = r.*cos(phi)
-y = r.*sin(phi)
-radi = 0.4+dr*(1:nr)-hdr
+X = 0.4+dr*arange(1, nr+1)-hdr
+Y = 0+dp*arange(nphi+1)+hdp
+r, phi = meshgrid(X, Y)
+
+x = r * cos(phi)
+y = r * sin(phi)
+radi = X
 
 beta = 1.5  # power law for density profile
