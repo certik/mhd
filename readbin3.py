e4d723c8893679c5db6867cd6ff8bdc0bf975f87
Ondrej Certik <ondrej@certik.cz>
2009-05-29 18:56:22 -0600

    a little more progress

--- readbin3.py
+++ readbin3.py
@@ -4,8 +4,12 @@ from struct import calcsize, unpack
 
 from numpy import arange, meshgrid, sin, cos, zeros
 
-def read(f, fmt):
-    return unpack(fmt, f.read(calcsize(fmt)))
+def fread(f, fmt):
+    u = unpack(fmt, f.read(calcsize(fmt)))
+    if len(fmt) == 1:
+        return u[0]
+    else:
+        return u
 
 nplanets = 1
 nr = 800
@@ -37,10 +41,21 @@ for n in [1, 2, 3]:
     phip = zeros(nplanets)
     vx = zeros(nplanets)
     vy = zeros(nplanets)
-    n4 = read(f, 4*"i")
+    mplanet = zeros(nplanets)
+    roche = zeros(nplanets)
+    n4 = fread(f, 4*"i")
     if not ((nplanets == n4[0]) and (nr == n4[1]) and (nphi == n4[2]) and \
             (nvar == n4[3])):
         print "The nplanets, nr, nphi, nvar should be:", n4[0], n4[1], n4[2], \
             n4[3]
         print "but are:", nplanets, nr, nphi, nvar
         sys.exit()
+    turn = round(fread(f, "f")/2/pi)
+    cs = fread(f, "f")
+    for np in range(nplanets):
+        rp[np] = fread(f, "f")
+        phip[np] = fread(f, "f")
+        vx[np] = fread(f, "f")
+        vy[np] = fread(f, "f")
+        mplanet[np] = fread(f, "f")
+        roche[np] = (mplanet[np]/3.0/(1+mplanet[np]))**(1./3) * rp[np]
