5df1283c6e0c02feba78781e91390e628a51053c
Ondrej Certik <ondrej@certik.cz>
2009-06-05 16:07:09 -0600

    Docstring added

--- plot.py
+++ plot.py
@@ -1,3 +1,10 @@
+"""
+Reads the pickled arrays (X, Y, C) and does a plot using pcolor() and saves all
+frames as png files. To create an animation, use for example:
+
+mencoder "mf://density0*.png" -mf fps=20 -o density.avi -ovc lavc -lavcopts vcodec=mpeg4
+"""
+
 from cPickle import load
 
 from pylab import pcolor, pcolormesh, show, savefig, clf
