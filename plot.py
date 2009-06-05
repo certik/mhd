e39ce489b9aecbb6f0f66b08bfd638ca4ce79fc5
Ondrej Certik <ondrej@certik.cz>
2009-06-05 16:04:20 -0600

    clear the figure between frames (frees memory)

--- plot.py
+++ plot.py
@@ -1,13 +1,16 @@
 from cPickle import load
 
-from pylab import pcolor, pcolormesh, show, savefig
+from pylab import pcolor, pcolormesh, show, savefig, clf
 
+#f = open("../plot217to249")
+#f = open("../plot191to215")
 #f = open("../plot157to189")
 f = open("../plot1to155")
 i_start = 1
 data = load(f)
 for i in range(len(data)):
     j = i_start + i
+    clf()
     X, Y, C = data[i]
     print "it=%d" % j
     print "  pcolor..."
