2582ef40cfbc70c7957ba2653cd1a9d29e4197d5
Ondrej Certik <ondrej@certik.cz>
2009-06-05 14:17:20 -0600

    pcolormesh() is a lot faster

--- test_pcolor.py
+++ test_pcolor.py
@@ -1,11 +1,20 @@
+from timeit import default_timer as clock
 from numpy import meshgrid, arange, pi, sin, cos
-from pylab import pcolor, show
+from pylab import pcolor, show, pcolormesh, savefig
 
-r, phi = meshgrid(arange(0, 10., 1), arange(0, 2*pi, 0.1))
+r, phi = meshgrid(arange(0, 10., 0.01), arange(0, 2*pi, 0.001))
 
 f = r**2
 
 x = r * cos(phi)
 y = r * sin(phi)
-pcolor(x, y, f)
-show()
+t = clock()
+#pcolor(x, y, f)
+pcolormesh(x, y, f)
+t = clock() - t
+print "pcolor:", t
+t = clock()
+savefig("c.png")
+t = clock() - t
+print "save:  ", t
+#show()
