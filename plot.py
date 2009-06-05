bca55ba84b9d2f4434c89a210a827c113b7837e6
Ondrej Certik <ondrej@certik.cz>
2009-06-05 16:27:16 -0600

    mpl plot is being fixed

--- plot.py
+++ plot.py
@@ -7,21 +7,19 @@ mencoder "mf://density0*.png" -mf fps=20 -o density.avi -ovc lavc -lavcopts vcod
 
 from cPickle import load
 
-from pylab import pcolor, pcolormesh, show, savefig, clf
+from pylab import pcolor, pcolormesh, show, savefig, clf, colorbar, gca
 
-#f = open("../plot217to249")
+f = open("../plot217to249")
 #f = open("../plot191to215")
 #f = open("../plot157to189")
-f = open("../plot1to155")
-i_start = 1
+#f = open("../plot1to155")
+print "load"
 data = load(f)
-for i in range(len(data)):
-    j = i_start + i
-    clf()
-    X, Y, C = data[i]
-    print "it=%d" % j
-    print "  pcolor..."
-    pcolormesh(X, Y, C)
-    print "  save..."
-    savefig("density%04d.png" % j)
-    print "  done."
+X, Y, C = data[-1]
+print "pcolor"
+print C.shape
+pcolormesh(X, Y, C)
+#colorbar()
+gca().set_aspect("equal")
+print "show"
+show()
