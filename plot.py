8cff8e18253480f1a6b4cfab33b87262e9a48c83
Ondrej Certik <ondrej@certik.cz>
2009-06-05 14:19:09 -0600

    plot.py made 150x faster by using pcolormesh()

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 from numpy import load
 
-from pylab import pcolor, show, savefig
+from pylab import pcolor, pcolormesh, show, savefig
 
 f = load("/tmp/plot.npz")
 X = f["X1"]
@@ -10,6 +10,6 @@ print X.shape
 print Y.shape
 print C.shape
 print "pcolor"
-pcolor(X, Y, C)
+pcolormesh(X, Y, C)
 print "save"
 savefig("density.png")
