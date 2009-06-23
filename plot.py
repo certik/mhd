e64b9ccc4da696274f702ded402c7d38a7105ff4
Ondrej Certik <ondrej@certik.cz>
2009-06-23 16:40:21 -0600

    Debugging statements added

--- plot.py
+++ plot.py
@@ -1,8 +1,5 @@
 """
-Reads the pickled arrays (X, Y, C) and does a plot using pcolor() and saves all
-frames as png files. To create an animation, use for example:
-
-mencoder "mf://density0*.png" -mf fps=20 -o density.avi -ovc lavc -lavcopts vcodec=mpeg4
+Reads data from data.h5 file and plots it using pcolor().
 """
 
 import sys
@@ -13,15 +10,25 @@ from pylab import pcolor, pcolormesh, show, savefig, clf, colorbar, gca
 
 from tables import IsDescription, openFile, Float64Col
 
+print "opening data"
 h5 = openFile("../data.h5")
 Cdata = h5.root.data.Cdata
+print "  done."
 
 C_min = 0.174630617373
 C_max = 2.23176607373
 
+print "read X, Y:"
 X = array(h5.root.data.X)
 Y = array(h5.root.data.Y)
+print "  done."
+
+print "read C:"
 C = Cdata.cols.C[0]
+print "  done."
+
+C_min = C.min()
+C_max = C.max()
 
 print "pcolor"
 print C.shape
@@ -29,5 +36,7 @@ print "min/max", C.min(), C.max()
 pcolormesh(X, Y, C, vmin=C_min, vmax=C_max)
 colorbar()
 gca().set_aspect("equal")
+print "savefig"
+savefig("a.png")
 print "show"
 show()
