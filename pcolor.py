6bd967b011e88e8d5f451faa3024339a93486917
Ondrej Certik <ondrej@certik.cz>
2009-07-08 16:16:57 -0600

    separate things

--- pcolor.py
+++ pcolor.py
@@ -4,7 +4,7 @@ Implements matplotlib's pcolor() like function, that produces a vtk file.
 
 import visit_writer
 
-def pcolor_unstructured(X, Y, C):
+def pts_connectivity(X, Y):
     """
     Does "pcolor" plot and returns points, connectivity and zonal & nodal data.
     """
@@ -25,6 +25,22 @@ def pcolor_unstructured(X, Y, C):
                         visit_writer.quad,
                         p(i, j), p(i+1, j), p(i+1, j+1), p(i, j+1),
                     ) )
+    return pts, connectivity
+
+def zonal_nodal(w, h, C):
+    """
+    Does "pcolor" plot and returns points, connectivity and zonal & nodal data.
+    """
+    zonal = []
+    nodal = []
+    for i in range(w):
+        for j in range(h):
+            if i + 1 < w and j + 1 < h:
                 zonal.append(C[i, j])
             nodal.append(C[i, j])
+    return zonal, nodal
+
+def pcolor_unstructured(X, Y, C):
+    pts, connectivity = pts_connectivity(X, Y)
+    zonal, nodal = zonal_nodal(X.shape[0], X.shape[1], C)
     return pts, connectivity, zonal, nodal
